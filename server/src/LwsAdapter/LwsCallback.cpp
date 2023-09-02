8c314981acad3d979f57ccaa47a94de160d87422
Volodymyr Lotoshko <vlotoshko@gmail.com>
2023-09-02 13:14:14 +0300

    Removed callbacks debug info

--- server/src/LwsAdapter/LwsCallback.cpp
+++ server/src/LwsAdapter/LwsCallback.cpp
@@ -39,83 +39,6 @@ auto getSessionPath(lws* wsInstance) -> Path
     return static_cast<Path>(buffer.data());
 }
 
-auto reasonToString(lws_callback_reasons reason) -> std::string
-{
-    auto alpha = std::string{" ("}.append(std::to_string(reason)).append(")");
-
-    switch (reason)
-    {
-    case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
-        return std::string{"LWS_CALLBACK_CLIENT_CONNECTION_ERROR"}.append(alpha);
-    case LWS_CALLBACK_CLIENT_FILTER_PRE_ESTABLISH:
-        return std::string{"LWS_CALLBACK_CLIENT_FILTER_PRE_ESTABLISH"}.append(alpha);
-    case LWS_CALLBACK_CLIENT_ESTABLISHED:
-        return std::string{"LWS_CALLBACK_CLIENT_ESTABLISHED"}.append(alpha);
-    case LWS_CALLBACK_CLIENT_CLOSED:
-        return std::string{"LWS_CALLBACK_CLIENT_CLOSED"}.append(alpha);
-    case LWS_CALLBACK_CLIENT_RECEIVE:
-        return std::string{"LWS_CALLBACK_CLIENT_RECEIVE"}.append(alpha);
-    case LWS_CALLBACK_CLIENT_WRITEABLE:
-        return std::string{"LWS_CALLBACK_CLIENT_WRITEABLE"}.append(alpha);
-    case LWS_CALLBACK_CLIENT_APPEND_HANDSHAKE_HEADER:
-        return std::string{"LWS_CALLBACK_CLIENT_APPEND_HANDSHAKE_HEADER"}.append(alpha);
-    case LWS_CALLBACK_CLOSED_HTTP:
-        return std::string{"LWS_CALLBACK_CLOSED_HTTP"}.append(alpha);
-    case LWS_CALLBACK_PROTOCOL_INIT:
-        return std::string{"LWS_CALLBACK_PROTOCOL_INIT"}.append(alpha);
-    case LWS_CALLBACK_PROTOCOL_DESTROY:
-        return std::string{"LWS_CALLBACK_PROTOCOL_DESTROY"}.append(alpha);
-    case LWS_CALLBACK_FILTER_NETWORK_CONNECTION:
-        return std::string{"LWS_CALLBACK_FILTER_NETWORK_CONNECTION"}.append(alpha);
-    case LWS_CALLBACK_WSI_CREATE:
-        return std::string{"LWS_CALLBACK_WSI_CREATE"}.append(alpha);
-    case LWS_CALLBACK_SERVER_NEW_CLIENT_INSTANTIATED:
-        return std::string{"LWS_CALLBACK_SERVER_NEW_CLIENT_INSTANTIATED"}.append(alpha);
-    case LWS_CALLBACK_HTTP_CONFIRM_UPGRADE:
-        return std::string{"LWS_CALLBACK_HTTP_CONFIRM_UPGRADE"}.append(alpha);
-    case LWS_CALLBACK_HTTP_BIND_PROTOCOL:
-        return std::string{"LWS_CALLBACK_HTTP_BIND_PROTOCOL"}.append(alpha);
-    case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
-        return std::string{"LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION"}.append(alpha);
-    case LWS_CALLBACK_ADD_HEADERS:
-        return std::string{"LWS_CALLBACK_ADD_HEADERS"}.append(alpha);
-    case LWS_CALLBACK_ESTABLISHED:
-        return std::string{"LWS_CALLBACK_ESTABLISHED"}.append(alpha);
-    case LWS_CALLBACK_SERVER_WRITEABLE:
-        return std::string{"LWS_CALLBACK_SERVER_WRITEABLE"}.append(alpha);
-    case LWS_CALLBACK_RECEIVE:
-        return std::string{"LWS_CALLBACK_RECEIVE"}.append(alpha);
-    case LWS_CALLBACK_GET_THREAD_ID:
-        return std::string{"LWS_CALLBACK_GET_THREAD_ID"}.append(alpha);
-    case LWS_CALLBACK_EVENT_WAIT_CANCELLED:
-        return std::string{"LWS_CALLBACK_EVENT_WAIT_CANCELLED"}.append(alpha);
-    case LWS_CALLBACK_WS_PEER_INITIATED_CLOSE:
-        return std::string{"LWS_CALLBACK_WS_PEER_INITIATED_CLOSE"}.append(alpha);
-    case LWS_CALLBACK_CLOSED:
-        return std::string{"LWS_CALLBACK_CLOSED"}.append(alpha);
-    case LWS_CALLBACK_WSI_DESTROY:
-        return std::string{"LWS_CALLBACK_WSI_DESTROY"}.append(alpha);
-    case LWS_CALLBACK_ADD_POLL_FD:
-        return std::string{"LWS_CALLBACK_ADD_POLL_FD"}.append(alpha);
-    case LWS_CALLBACK_DEL_POLL_FD:
-        return std::string{"LWS_CALLBACK_DEL_POLL_FD"}.append(alpha);
-    case LWS_CALLBACK_CHANGE_MODE_POLL_FD:
-        return std::string{"LWS_CALLBACK_CHANGE_MODE_POLL_FD"}.append(alpha);
-    case LWS_CALLBACK_LOCK_POLL:
-        return std::string{"LWS_CALLBACK_LOCK_POLL"}.append(alpha);
-    case LWS_CALLBACK_UNLOCK_POLL:
-        return std::string{"LWS_CALLBACK_UNLOCK_POLL"}.append(alpha);
-    case LWS_CALLBACK_OPENSSL_LOAD_EXTRA_CLIENT_VERIFY_CERTS:
-        return std::string{"LWS_CALLBACK_OPENSSL_LOAD_EXTRA_CLIENT_VERIFY_CERTS"}.append(alpha);
-    case LWS_CALLBACK_OPENSSL_LOAD_EXTRA_SERVER_VERIFY_CERTS:
-        return std::string{"LWS_CALLBACK_OPENSSL_LOAD_EXTRA_SERVER_VERIFY_CERTS"}.append(alpha);
-    case LWS_CALLBACK_VHOST_CERT_AGING:
-        return std::string{"LWS_CALLBACK_VHOST_CERT_AGING"}.append(alpha);
-    default:
-        return std::string{"Unknown"}.append(alpha);
-    }
-}
-
 auto sendMessage(lws* wsInstance, const Message& message) -> bool
 {
     // C-style cast to convert from const char* to unsigned char*
@@ -137,7 +60,6 @@ auto lwsCallback_v1(
         size_t len/*length*/)
 -> int
 {
-    std::cout << "srv LwsCallback reason: " << reasonToString(reason) << std:: endl;
     auto& callbackContext = getCallbackContext(wsInstance);
     auto eventHandler = callbackContext.getEventHandler();
     auto sessionId = getSessionId(wsInstance);
