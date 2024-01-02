c61a0123121975f655549963790b538ccf6d8f90
Volodymyr Lotoshko <vlotoshko@gmail.com>
2024-01-02 17:06:38 +0300

    Added cmake package config files

---
+++ client/cmake/install.cmake
@@ -0,0 +1,35 @@
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+set(package lwsppClient)
+
+set(CMAKE_CONFIG_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${package}")
+
+configure_package_config_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/cmake/${package}Config.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/${package}Config.cmake
+    INSTALL_DESTINATION ${CMAKE_CONFIG_DESTINATION}
+)
+
+write_basic_package_version_file(
+    ${CMAKE_CURRENT_BINARY_DIR}/${package}ConfigVersion.cmake
+    COMPATIBILITY SameMajorVersion
+)
+
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/lwspp DESTINATION include)
+
+install(FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/${package}Config.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/${package}ConfigVersion.cmake
+    DESTINATION ${CMAKE_CONFIG_DESTINATION}
+)
+
+install(EXPORT lwsppClientTargets
+    NAMESPACE lwspp::
+    DESTINATION ${CMAKE_CONFIG_DESTINATION}
+)
+
+install(TARGETS ${TARGET_LWSPP_CLIENT_STATIC} ${TARGET_LWSPP_CLIENT_SHARED}
+    EXPORT lwsppClientTargets
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+)
